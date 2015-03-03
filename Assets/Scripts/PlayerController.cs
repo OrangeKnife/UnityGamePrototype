6066886652c57d7ddefa806887a6fcd430fb0492
dekhadmai <bank_kuna@hotmail.com>
2015-03-02 21:24:50 -0500

    - Try to figure out SVN error : Invalid PROPPATCH property
    - Added TestScene.unity
    - Added Prefabs - Player, Platform
    - Added PlayerController
    - Added more folders

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D tmpRigidBody;
+	private float jumpForce = 500.0f;
+	private float walkForce = 10.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		tmpRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float horizontal, vertical;
+
+		horizontal = Input.GetAxisRaw ("Horizontal");
+		//vertical = Input.GetAxisRaw ("Vertical");
+		vertical = (Input.GetButtonDown ("Jump") ? 1.0f : 0.0f );
+
+		HandleInput (horizontal * walkForce, vertical * jumpForce);
+	}
+
+	void HandleInput(float horizontal, float vertical)
+	{
+		Vector2 tmpVec;
+		tmpVec = new Vector2 (horizontal, vertical);
+		tmpRigidBody.AddForce ( tmpVec );
+	}
+}
