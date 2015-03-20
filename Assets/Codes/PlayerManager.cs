0bc941659b4459aae608073120ce559193b535c2
nueng nueng <nuengcusci@gmail.com>
2015-03-20 23:56:25 +0700

    add score showing on game, using onGUI. may change to other function if there is a better place

--- Assets/Codes/PlayerManager.cs
+++ Assets/Codes/PlayerManager.cs
@@ -4,9 +4,16 @@ using UnityEngine.UI;
 public class PlayerManager : MonoBehaviour {
 
 	private int playerScore = 0;
+	private int MAXSCORELENGTH = 100;
 	// Use this for initialization
+	void OnGUI () {
+		GUIStyle myStyle = new GUIStyle(GUI.skin.textField);
+		myStyle.alignment = TextAnchor.MiddleRight;
+		myStyle.fontSize = 25;
+		GUI.TextField (new Rect (10, 10, 100, 30), playerScore.ToString(), MAXSCORELENGTH, myStyle );
+	}
 	void Start () {
-	
+
 	}
 
 	public void addPlayerScore(int score) {
@@ -18,9 +25,11 @@ public class PlayerManager : MonoBehaviour {
 		playerScore = 0;
 		UpdateSocre (playerScore);
 	}
-
 	public int getPlayerScore() {
 		return playerScore;
+	}
+	public void setPlayerScore(int score) {
+		playerScore = score;
 	}
 	// Update is called once per frame
 	void Update () {
