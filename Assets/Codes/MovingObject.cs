af3a46fe7b13f63742dc7ab030a97b2e9f1a8619
dekhadmai <bank_kuna@hotmail.com>
2015-03-27 00:49:58 -0400

    - Added MovingObject.cs for moving spike! good luck with that - Added more sections

---
+++ Assets/Codes/MovingObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour {
+
+	public float MoveSpeed = 0.5f;
+	public Vector2 OffsetPointA; // first destination
+	public Vector2 OffsetPointB; // second destination
+	public bool bMoveOnce = false;
+
+	private float currentTimer;
+	private Vector2 tmpOffset;
+
+	// Use this for initialization
+	void Start () {
+		currentTimer = 0.0f;
+		OffsetPointA = (Vector2)transform.position + OffsetPointA;
+		OffsetPointB = (Vector2)transform.position + OffsetPointB;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		currentTimer += Time.deltaTime;
+		transform.position = Vector2.Lerp(OffsetPointA, OffsetPointB, currentTimer * MoveSpeed);
+
+		if (!bMoveOnce)
+		{
+			// move back and forth between pointA and pointB
+			if (transform.position.x == OffsetPointB.x && transform.position.y == OffsetPointB.y)
+			{
+				currentTimer = 0.0f;
+				tmpOffset = OffsetPointA;
+				OffsetPointA = OffsetPointB;
+				OffsetPointB = tmpOffset;
+			}
+		}
+	}
+
+//	var pointB : Vector3;
+//	
+//	function Start () { var pointA = transform.position; while (true) { yield MoveObject(transform, pointA, pointB, 3.0); yield MoveObject(transform, pointB, pointA, 3.0); } }
+//	
+//	function MoveObject (thisTransform : Transform, startPos : Vector3, endPos : Vector3, time : float) { var i = 0.0; var rate = 1.0/time; while (i < 1.0) { i += Time.deltaTime * rate; thisTransform.position = Vector3.Lerp(startPos, endPos, i); yield; } }
+}
