44b7b21ffdffaa40d1e0de5f4b5bc2eb2a258045
nueng nueng <nuengcusci@gmail.com>
2015-04-04 00:33:14 +0700

    Commit time use for ability

--- Assets/Codes/AbilityBase.cs
+++ Assets/Codes/AbilityBase.cs
@@ -8,17 +8,17 @@ public class AbilityBase : MonoBehaviour {
 	private static float ACTIVETIMER = 2.0f;
 	private float active_remain = ACTIVETIMER;
 	private float timer = CDTIMER;
+	private System.DateTime startAbilityTime;
+	private System.DateTime startCoolDownTime;
+
 	void Start()
 	{
+		print ("start abi");
 		EnableAbilityPassive();
 	}
 	void Update()
 	{
-		timer -= Time.deltaTime;
-		active_remain -= Time.deltaTime;
-		
-		print ("cooldown remain timer: " + timer);
-		print ("active_remain timer: " + active_remain);
+		print("ramain cooldown : "+GetRemainingCooldown());
 	}
 	public virtual bool IsActiveAbility()
 	{
@@ -27,18 +27,40 @@ public class AbilityBase : MonoBehaviour {
 	}
 	public virtual void EnableAbilityActive()
 	{
-		print("Enable Active Base");
-		bActiveAbility = true;
+		if (GetRemainingCooldown () <= 0) {
+			print("Enable Active Base");
+			bActiveAbility = true;
+			startAbilityTime = System.DateTime.Now;
+			startCoolDownTime = System.DateTime.Now;
+		}
+
+
 	}
 	public virtual void DisableAbilityActive()
 	{
 		print("Disable Active Base");
 		bActiveAbility = false;
+
+		//print ("time span = " + ts);
 	}
-	public virtual void EnableAbilityPassive(){}
-	public virtual void DisableAbilityPassive(){}
-	public virtual float GetTotalCooldown(){return CDTIMER;}
-	public virtual float GetRemainingCooldown(){return timer;}
-	public virtual float GetActiveTotalDuration(){return ACTIVETIMER;}
-	public virtual float GetActiveRemainingDuration(){return active_remain;}
+	public virtual void EnableAbilityPassive() {
+
+	}
+	public virtual void DisableAbilityPassive() {
+	}
+	public virtual float GetTotalCooldown() {
+		return CDTIMER;
+	}
+	public virtual float GetRemainingCooldown() {
+		System.TimeSpan _currentCDTime = System.DateTime.Now - startCoolDownTime;
+		print ("test tim  e" +startCoolDownTime);
+		return System.Convert.ToSingle(_currentCDTime.TotalSeconds);
+	}
+	public virtual float GetActiveTotalDuration() {
+		return ACTIVETIMER;
+	}
+	public virtual float GetActiveRemainingDuration() {
+		System.TimeSpan _currentActiveTime = System.DateTime.Now - startAbilityTime;
+		return System.Convert.ToSingle(_currentActiveTime.TotalSeconds);
+	}
 }
