b6d4e6089415b6725ee9bf6e2d9c416fc05b1279
OrangeKnife <evisd21@gmail.com>
2015-04-11 06:37:23 -0700

    added 5 sections: Disappearing and Appearing objects

---
+++ Assets/Codes/Appearing.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Appearing : MonoBehaviour {
+	public bool AppearOnEnter = true;
+	public float Delay = 1.5f;
+	public bool WantFading = true;
+	public float FadingSpeed = 1f;
+	bool bDoAppearing;
+	
+	SpriteRenderer myRenderer;
+	BoxCollider2D box;
+	// Use this for initialization
+	void Start () {
+		myRenderer = GetComponent<SpriteRenderer> ();
+		Color currentColor = myRenderer.color;
+		myRenderer.color = new Color (currentColor.r, currentColor.g, currentColor.b, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myRenderer && bDoAppearing && WantFading) {
+			Color currentColor = myRenderer.color;
+			if(currentColor.a < 1)
+				myRenderer.color = new Color(currentColor.r,currentColor.g,currentColor.b,currentColor.a + FadingSpeed * Time.deltaTime);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (AppearOnEnter && collider.gameObject.tag == "Player"  && !bDoAppearing) {
+			Invoke("DoAppearing", Delay);
+			bDoAppearing = true;
+		}
+	}
+	
+	void DoAppearing()
+	{
+		gameObject.layer = 9;//9 == level
+		GetComponent<BoxCollider2D> ().isTrigger = false;
+	}
+}
---
+++ Assets/Codes/Disappearing.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disappearing : MonoBehaviour {
+	public bool DisappearOnStand = true;
+	public float Delay = 1.5f;
+	public bool WantFading = true;
+	public float FadingSpeed = 1f;
+	bool bDoDisappearing;
+
+	SpriteRenderer myRenderer;
+	// Use this for initialization
+	void Start () {
+		myRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myRenderer && bDoDisappearing && WantFading) {
+			Color currentColor = myRenderer.color;
+			if(currentColor.a > 0)
+				myRenderer.color = new Color(currentColor.r,currentColor.g,currentColor.b,currentColor.a - FadingSpeed * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (DisappearOnStand && collider.gameObject.tag == "Player" && collider.transform.position.y > transform.position.y && !bDoDisappearing) {
+			Invoke("DoDisappearing", Delay);
+			bDoDisappearing = true;
+		}
+	}
+
+	void DoDisappearing()
+	{
+		gameObject.SetActive(false);
+	}
+}
