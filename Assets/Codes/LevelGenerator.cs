03b6dda71d284c3b76736638ed58546fe158f9e0
dekhadmai <bank_kuna@hotmail.com>
2015-04-15 01:57:18 -0400

    - Added DifficultySystem in LevelGenerator

--- Assets/Codes/LevelGenerator.cs
+++ Assets/Codes/LevelGenerator.cs
@@ -30,6 +30,65 @@ public class LevelGenerator : MonoBehaviour {
 	private LinkedList<GameObject> SpawnedSectionList;
 	private float LastSectionBeginPosition;
 	private int MAXSECTIONS = 7;
+
+
+	/// <summary>
+	/// Difficulty related
+	/// </summary>
+	public struct SectionPicker
+	{
+		public float[] PercentByDifficulty; // 0=wtf, 1=hard, 2=normal, 3=easy
+
+		public SectionPicker(float wtf, float hard, float normal, float easy)
+		{
+			PercentByDifficulty = new float[4];
+			PercentByDifficulty[0] = wtf;
+			PercentByDifficulty[1] = hard;
+			PercentByDifficulty[2] = normal;
+			PercentByDifficulty[3] = easy;
+		}
+
+		// 0=wtf, 1=hard, 2=normal, 3=easy
+		public int GetSectionType(int Difficulty) 
+		{
+			float tmpRand;
+
+			tmpRand = Random.Range(0.0f, 100.0f);
+			for (int i = 0; i < 3; ++i)
+			{
+				if (tmpRand < PercentByDifficulty[i] * Difficulty)
+				{
+					print ("PickedSection: " + GetSectionName(i));
+					return i;
+				}
+				tmpRand = Random.Range(0.0f, 100.0f);
+			}
+
+			// always at least return 'easy'
+			print ("PickedSection: " + GetSectionName(3));
+			return 3;
+		}
+
+		private string GetSectionName(int num)
+		{
+			if (num == 3)
+				return "Easy";
+			else if (num == 2)
+				return "Normal";
+			else if (num == 1)
+				return "Hard";
+			else if (num == 0)
+				return "WTF";
+			else 
+				return "Error";
+		}
+	}
+
+	private int NumberOfGeneratedSection;
+	private int DifficultyMod = 5; // Difficulty = NumberOfGeneratedSection % DifficultyMod
+	private int SurpriseSectionMod = 10; // special section is picked every X sections
+	private SectionPicker NormalPicker;
+	private SectionPicker SurprisePicker;
 	
 	GameManager gameMgr;
 	// Use this for initialization
@@ -51,8 +110,11 @@ public class LevelGenerator : MonoBehaviour {
 	{
 		if (SpawnedSectionList == null)
 			SpawnedSectionList = new LinkedList<GameObject>();
-
+		
+		NumberOfGeneratedSection = 0;
 		currentLevel = SectionArray_Easy;
+		NormalPicker = new SectionPicker(0.5f, 2.0f, 10.0f, 9999.0f);
+		SurprisePicker = new SectionPicker(10.0f, 9999.0f, 9999.0f, 9999.0f);
 
 		///// grab player
 		//player = GameObject.FindGameObjectWithTag ("Player");
@@ -90,6 +152,7 @@ public class LevelGenerator : MonoBehaviour {
 			// init first 2-3 sections here
 			for (int i = 0; i < MAXSECTIONS; ++i)
 			{
+				currentLevel = GetSectionArray( NormalPicker.GetSectionType( GetDifficulty() ) );
 				SpawnSection(currentLevel[Random.Range(0,currentLevel.Count)]);
 			}
 		}
@@ -103,6 +166,16 @@ public class LevelGenerator : MonoBehaviour {
 		if (player.transform.position.x > SpawnedSectionList.Last.Previous.Value.GetComponent<LevelSectionScript>().boundingBox.min.x)
 		{
 			///// player enters last section
+			if (NumberOfGeneratedSection % SurpriseSectionMod != 0)
+			{
+				currentLevel = GetSectionArray( NormalPicker.GetSectionType( GetDifficulty() ) );
+			}
+			else
+			{
+				// pick surprise section
+				print ("Surprise!");
+				currentLevel = GetSectionArray( SurprisePicker.GetSectionType( GetDifficulty() ) );
+			}
 			SpawnSection(currentLevel[Random.Range(0,currentLevel.Count)]);
 		}
 
@@ -117,7 +190,11 @@ public class LevelGenerator : MonoBehaviour {
 	void SpawnSection(GameObject template)
 	{
 		///// spawn template
+		if (template == null)
+			return;
+
 		GameObject randomSection = Instantiate(template);
+		NumberOfGeneratedSection++;
 
 		///// calculate bounding box for each template (for future calculation)
 		Bounds tmp = new Bounds();
@@ -155,5 +232,24 @@ public class LevelGenerator : MonoBehaviour {
 		SpawnedSectionList.AddLast(randomSection);
 
 		LastSectionBeginPosition = SpawnedSectionList.Last.Value.GetComponent<LevelSectionScript>().boundingBox.min.x;
+	}
+
+	public int GetDifficulty()
+	{
+		return (NumberOfGeneratedSection % DifficultyMod) +1;
+	}
+
+	private List<GameObject> GetSectionArray(int num)
+	{
+		if (num == 3)
+			return SectionArray_Easy;
+		else if (num == 2)
+			return SectionArray_Normal;
+		else if (num == 1)
+			return SectionArray_Hard;
+		else if (num == 0)
+			return SectionArray_Wtf;
+		else
+			return null; // error
 	}
 }
