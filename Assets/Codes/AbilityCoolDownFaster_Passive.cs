859376032ce0982453959fd5dc21f0377308f460
nueng nueng <nuengcusci@gmail.com>
2015-04-29 00:25:12 +0700

    Commit ability cooldown and score multiplier passive

---
+++ Assets/Codes/AbilityCoolDownFaster_Passive.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityCoolDownFaster_Passive : AbilityBase {
+
+	// Use this for initialization
+	private PlayerController playerCtrl;
+	private AbilityBase abilityBase;
+	
+	public override void EnableAbilityPassive()
+	{
+		abilityBase = GetComponent<AbilityBase>();
+		abilityBase.setCDMultipler(abilityBase.getCDMultipler()*0.3f);
+		
+		base.EnableAbilityPassive();
+	}
+	
+	public override Sprite GetIcon()
+	{
+		if (IconSprite == null)
+			IconSprite = Resources.Load<Sprite>("Ability/Icon/AbilityIcon_2");
+		
+		return IconSprite;
+	}
+}
---
+++ Assets/Codes/AbilityScoreMultiplier_Passive.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbilityScoreMultiplier_Passive : AbilityBase {
+
+	private LevelSectionScript lvSection;
+	private PlayerManager playerMan;
+	
+	public override void EnableAbilityPassive()
+	{
+		lvSection = GetComponent<LevelSectionScript>();
+		lvSection.setScoreMultiplier(lvSection.getScoreMultiplier()*2f);
+		print ("score : " + lvSection.getScoreMultiplier () * 2f);
+		base.EnableAbilityPassive();
+	}
+	
+	public override Sprite GetIcon()
+	{
+		if (IconSprite == null)
+			IconSprite = Resources.Load<Sprite>("Ability/Icon/AbilityIcon_2");
+		
+		return IconSprite;
+	}
+}
