c0a62d81dcdf8bbfcef7b64e4e9cddfd304ff68b
nueng nueng <nuengcusci@gmail.com>
2015-05-17 23:45:20 +0700

    Commit spring and fire ring sections & prefabs

---
+++ Assets/Codes/Spring.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+	private PlayerController _player;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		/*if (AppearOnEnter && collider.gameObject.tag == "Player"  && !bDoAppearing) {
+			Invoke("DoAppearing", Delay);
+			bDoAppearing = true;
+		}*/
+		_player = GetComponent<PlayerController>();
+
+		bool ButtonJumpHold = false;
+		if ( Input.GetButton ("Jump") )
+		{
+			ButtonJumpHold = true;
+		}
+		//print (_player.getJumpForce ());
+		float springForce = 1500 * 1.5f;
+		Vector2 tmpVec = new Vector2 (0.0f, springForce);
+		collider.attachedRigidbody.AddForce (tmpVec);
+			
+	}
+}
